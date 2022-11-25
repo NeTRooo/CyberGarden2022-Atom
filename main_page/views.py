60ed220d9e610e4e0fe38d2ee6f5c70f738494e9
DenSSS <dimon.yablonovskiyy@mail.ru>
2022-11-25 19:44:47 +0300

    1

--- main_page/views.py
+++ main_page/views.py
@@ -3,12 +3,19 @@ from django.http import HttpRequest, HttpResponse, JsonResponse, HttpResponseRed
 from django.contrib.auth import logout
 import json
 import requests
+from django.views.decorators import gzip
+from django.http import StreamingHttpResponse
+import cv2
+import threading
 
 def main_page(request):
     return render(request, 'main_page/main_page.html')
+<<<<<<< HEAD
 
 def forms_page(request):
     return render(request, 'main_page/forms.html')
 
 def quiz_page(request):
     return render(request, 'main_page/quiz.html')
+=======
+>>>>>>> 22b4b0c41a80199f62f2754ee0bc0821bf107e05
