8c96d8c37a405d45fbf42e90145b195766b76565
linum1337 <lebishova.elena@mail.ru>
2022-11-25 18:53:09 +0300

    добавил инициализацию вебки

--- main_page/views.py
+++ main_page/views.py
@@ -3,8 +3,11 @@ from django.http import HttpRequest, HttpResponse, JsonResponse, HttpResponseRed
 from django.contrib.auth import logout
 import json
 import requests
+import cv2
 
 def main_home(request):
     return render(request, 'main_page/main_page.html')
 
-    
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
