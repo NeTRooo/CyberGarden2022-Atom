141f03c395d5c48a267a8afc5074c169d019ee77
linum1337 <lebishova.elena@mail.ru>
2022-11-26 04:37:33 +0300

    исправления в детекте

--- camera.py
+++ camera.py
@@ -36,25 +36,26 @@ def gen(camera):
     CONFIDENCE = 0.5
     SCORE_THRESHOLD = 0.5
     IOU_THRESHOLD = 0.5
-
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-    per_r = '10' + '%'
     font = cv2.FONT_HERSHEY_COMPLEX
-    percent_choise = 0
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    rand_lvl = [random.randrange(70, 100) for i in range(0, 50)]
+    frame_cntr = 0
     while True:
 
         ret, frame = video.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-
-
+        i = 0
         for (x, y, z, h) in faces:
-            percent_choise+=1
+            frame_cntr += 1
+
             cv2.rectangle(frame, (x, y), (x + z, y + h), (255, 0, 0), 2)
-            if percent_choise < 50:
-                continue
-            cv2.putText(frame, (f"Сходство с junior-разработчиком:{per_r}"),(10,500), font, 1.5,(255,255,255),2,cv2.LINE_AA)
+            if frame_cntr < 100:
+                cv2.putText(frame, f'Вы junior-разработчик на:{random.randrange(0, 100)}%', (x - 6, y), font, 0.7, (255, 255, 255), 2, cv2.LINE_AA)
+            else:
+                i += 1
+                cv2.putText(frame, f'Вы junior-разработчик на:{rand_lvl[i]}%', (x - 6, y), font, 0.7, (255, 255, 255), 2, cv2.LINE_AA)
+
 
         x, imag = cv2.imencode('.jpg', frame)
 
